8183444104cf28c72b81d100524ac1ae50fc2674
Peter Lyons <pete@peterlyons.com>
2017-09-30 19:33:52 -0600

    Promote dev to demo

--- terraform/demo/outputs.tf
+++ terraform/demo/outputs.tf
@@ -1,5 +1,5 @@
 output "api_url" {
-  value = "https://${aws_api_gateway_rest_api.hexagonal-lambda.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_deployment.api.stage_name}"
+  value = "${aws_api_gateway_deployment.api.invoke_url}"
 }
 
 output "api_id" {
