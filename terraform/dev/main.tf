0a9bcbb34d8e5bb13c2a8f1271b4bd31ceb59a26
Peter Lyons <pete@peterlyons.com>
2017-09-30 19:32:43 -0600

    Tweak terraform deps and outputs

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -13,6 +13,11 @@ resource "aws_api_gateway_deployment" "api" {
   rest_api_id = "${aws_api_gateway_rest_api.hexagonal-lambda.id}"
   stage_name  = "${var.deploy}"
 
+  depends_on = [
+    "module.get-hex-endpoint",
+    "module.post-up-endpoint",
+  ]
+
   # https://medium.com/coryodaniel/til-forcing-terraform-to-deploy-a-aws-api-gateway-deployment-ed36a9f60c1a
   variables {
     deployed_at = "${timestamp()}"
