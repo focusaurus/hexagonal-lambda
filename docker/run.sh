f9b3238872644df5531cc526e0ed4602c1b8e5bb
Peter Lyons <pete@reactioncommerce.com>
2018-10-01 10:43:55 -0600

    Remove v1 docker approach

--- docker/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$0")/.."
-source ./bin/lib/strict-mode.sh
-exec docker run --rm --interactive --tty \
-  --attach stdin --attach stdout --attach stderr \
-  --volume "${PWD}:/opt" \
-  --publish "127.0.0.1:9229:9229" \
-  hexagonal-lambda "$@"
